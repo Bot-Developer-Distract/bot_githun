fc8d79ffc43f61c113e8bb904a3874db9298e7d8
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2022-11-16 22:17:30 +0700

    Update anime.py

--- command/anime.py
+++ command/anime.py
@@ -7,7 +7,7 @@ from command.random_list import list_color
 class Anime(commands.Cog):
   config = {
       "name": "anime",
-      "desc": "ảnh hoặc gif anime theo từng category+)",
+      "desc": "gif anime theo từng category+)",
       "use": "<prefix>anime <category>\ncác category hiện có: baka, bite, blush, bored, cry, cuddle, dance, facepalm, feed, handhold, happy, highfive, hug, kick,kiss, laugh, pat, poke, pout, punch, shoot, shrug, slap, sleep, smile, smug, stare, think, thumbsup, tickle, wave, wink, yeet",
       "author": "Anh Duc(aki team)"
     }
